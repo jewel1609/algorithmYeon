5e8fdd19fb682d1013d118235cbdcc4a8c41e05c
김연 <jewel1609@naver.com>
2017-06-15 18:58:25 +0900

    커밋

---
+++ src/algorithm_0615/SolutionVirus.java
@@ -0,0 +1,13 @@
+package algorithm_0615;
+
+public class SolutionVirus {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test_Git_Hub");
+		System.out.println("Algorithm");
+		
+		
+	}
+	
+}
